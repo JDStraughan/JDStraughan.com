f8b0507061b1b2c98ea7fbabd2cf497ad56113e9
Jason Straughan <jdstraughan@gmail.com>
2011-06-10 19:39:49 -0500

    Added feed for all comments sitewide

--- application/classes/controller/feed.php
+++ application/classes/controller/feed.php
@@ -8,6 +8,17 @@ class Controller_Feed extends Controller {
 		
 		$slug = Request::instance()->param('slug');	
 		
+		if ($slug) {
+			$xml = $this->_commentsByPost($slug);
+		} else {
+			$xml = $this->_commentsAll();
+		}
+
+		$this->request->response = $xml;
+	}
+	
+	protected function _commentsByPost($slug) 
+	{
 		$post = new Model_Post();
 		$post = $post->get_by_slug($slug);
 
@@ -17,7 +28,7 @@ class Controller_Feed extends Controller {
 		}
 		
 		$info = array(
-			'title' => 'Recents comments for post: ' . $post->title,
+			'title' => 'Recents Comments for post: ' . $post->title,
 			'pubDate' => date("D, d M Y H:i:s T", $post->date_published),
 			'description' => 'Comments for post: ' . $post->title,
 			'link' => url::site("/post/$post->slug")); 
@@ -34,12 +45,41 @@ class Controller_Feed extends Controller {
 			$items[] = array(
 				'title' => "New comment by {$comment->name}",
 				'link' => url::site("post/{$post->slug}#comments"),
-				'description' => $comment->text,
+				'description' =>  nl2br($comment->text),
 				'pubDate' => date("D, d M Y H:i:s T", $comment->date_created)
 			);
 		} 
 		  
-		$xml = Feed::create($info, $items, 'rss2');
-		$this->request->response = $xml;
+		return Feed::create($info, $items, 'rss2');	
+	}
+	
+	protected function _commentsAll() 
+	{
+		$comments = new Model_Comment;
+		$comments = $comments->where('approved', '=', '1')
+				->order_by('date_created', 'DESC')
+				->limit(25)
+				->find_all();
+
+		$info = array(
+			'title' => 'Recents Comments on JDStraughan.com',
+			'pubDate' => date("D, d M Y H:i:s T"),
+			'description' => 'Comments from all posts on JDStraughan.com',
+			'link' => url::site()
+		); 
+		
+		$items = array();
+				
+		foreach ($comments as $comment) 
+		{
+			$items[] = array(
+				'title' => "New comment by {$comment->name} on post {$comment->post->title}",
+				'link' => url::site("post/{$comment->post->slug}#comments"),
+				'description' => nl2br($comment->text),
+				'pubDate' => date("D, d M Y H:i:s T", $comment->date_created)
+			);
+		}
+		
+		return Feed::create($info, $items, 'rss2');
 	}
 }
